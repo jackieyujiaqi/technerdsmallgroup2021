03cf22e3fc75e92df9122cab8a89e6880f98fa0b
devcmgm <49372756+devcmgm@users.noreply.github.com>
2021-02-21 17:08:46 -0600

    k

--- week1and2/src/App.js
+++ week1and2/src/App.js
@@ -1,28 +1,11 @@
 import React, { useState } from 'react'
 import './App.css'
-import Flower from './components/Flower'
-import ColorPalette from './components/ColorPalette'
 
 const App = () => {
-  const [fillColors, setFillColors] = useState(Array(22).fill('white'))
-  const [currentColor, setCurrentColor] = useState('blue')
-
-  const onFillColor = (i) => {
-    let newFillColors = fillColors.slice(0)
-    newFillColors[i] = currentColor
-    setFillColors(newFillColors)
-  }
 
   return (
     <div className="App">
-        <div className="heading">
-            <h1>Color in the place below, where you like to pray to Jesus.</h1>
-          <ColorPalette currentColor={currentColor} changeColor={setCurrentColor}/>
-        </div>
-      <div className= "flower">
-        Choose Avatar Page
-      </div>
-
+       <h1>Choose Avatar - First Page </h1>
     </div>
   )
 }
